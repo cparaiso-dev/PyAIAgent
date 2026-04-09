72d37ef26bc234c645a13ab4d12b292ed000d5e2
Christian Paraiso <cparaiso.dev@outlook.com>
2026-04-09 16:59:41 -0400

    add web search capability using Tavily

--- main.py
+++ main.py
@@ -9,26 +9,33 @@ from langchain_ollama import ChatOllama
 import gradio as gr
 from langgraph.checkpoint.sqlite import SqliteSaver
 import sqlite3
+#from langchain_community.tools import TavilySearchResults
+from langchain_tavily import TavilySearch
 
 from tools import chat, get_date
+from langchain_tools import parse_langchain_agent_response
 
 load_dotenv()
 
-GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
-
 conn = sqlite3.connect('chat_memory.db', check_same_thread=False)
 checkpointer = SqliteSaver(conn)
+
 llm = ChatOllama(model="qwen2.5:3b")
 
 system_prompt="""
-You are a helpful assistant. Answer all user queries.
-Use get_date tool if user is asking about today's date
+You are a helpful assistant.
+Use get_date tool when the user is asking about today's date.
+Use web search for up-to-date answers.
 """
 
-agent = create_agent(model=llm,
-                     tools=[get_date],
-                     system_prompt=system_prompt,
-                     checkpointer=checkpointer)
+search_tool = TavilySearch(max_results=2, topic="general")
+
+agent = create_agent(
+    model=llm,
+#    tools=[TavilySearch(max_results=5, topic="general"), get_date],
+    tools=[get_date, search_tool],
+    system_prompt=system_prompt,
+)
 
 with gr.Blocks() as demo:
     gr.Markdown("# AI Chatbot")
--- tools.py
+++ tools.py
@@ -1,6 +1,5 @@
 from datetime import datetime
-
-from langchain_tools import parse_langchain_agent_response
+#from langchain_tools import parse_langchain_agent_response
 
 
 def get_date():
@@ -14,7 +13,12 @@ def get_date():
 def chat(message, history, thread_id, agent):
     config = {'configurable': {"thread_id": thread_id}}
     agent_input = {"messages": [{"role": "user", "content": message}]}
+
     result = agent.invoke(agent_input, config)
 
+    """
     parsed = parse_langchain_agent_response(result)
     return parsed["final_answer"]
+    """
+
+    return result["messages"][-1].content
